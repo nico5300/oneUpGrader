8f1a8094c9ebf10370b48f29beee5621462e0cd5
Nico Greger <nico_5300@yahoo.de>
2017-04-07 20:51:36 +0200

    Gradle configs hochgeladen

---
+++ Client/src/main/java/de/hlg/oneUpGrader/client/Main.java
@@ -0,0 +1,10 @@
+package de.hlg.oneUpGrader.client;
+
+/**
+ * Created by nico on 07.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi. this is a test");
+    }
+}
