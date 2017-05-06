ee958c9a4d802a7b726514ad3174d0d2626a8b75
Nico Greger <nico_5300@yahoo.de>
2017-05-06 16:24:23 +0200

    Änderungen aus der Schule

--- Client/src/main/java/de/hlg/oneUpGrader/client/ui/controller/VerifizierenAuswahlController.java
+++ Client/src/main/java/de/hlg/oneUpGrader/client/ui/controller/VerifizierenAuswahlController.java
@@ -1,7 +1,25 @@
 package de.hlg.oneUpGrader.client.ui.controller;
 
+import de.hlg.oneUpGrader.client.ui.view.MainWindowView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  * Created by nico on 04.05.17.
  */
 public class VerifizierenAuswahlController {
+
+    @FXML
+    private Button btnBack;
+
+    public void onBackClicked(ActionEvent e) {
+        MainWindowView view = new MainWindowView();
+        Stage stage = (Stage) btnBack.getScene().getWindow();
+        Scene scene = new Scene(view.getView());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
