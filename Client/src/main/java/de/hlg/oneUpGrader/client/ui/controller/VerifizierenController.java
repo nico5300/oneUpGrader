4c9277cf893b59d795b7cf0127bcb3f477df2dac
JustGamerz <jakobkr8@gmail.com>
2017-05-08 15:26:13 +0200

    Ich habe ein paar neue fxml Dateien hinzugefügt. Es gibt jedoch das Problem, dass man von der MainWindowView nicht auf die VerifizierenAuswahlView wechseln kann obwohl die notwendige Methode im MainwindowController schon vorhanden ist...

---
+++ Client/src/main/java/de/hlg/oneUpGrader/client/ui/controller/VerifizierenController.java
@@ -0,0 +1,7 @@
+package main.java.de.hlg.oneUpGrader.client.ui.controller;
+
+/**
+ * Created by Jakob on 08.05.2017.
+ */
+public class VerifizierenController {
+}
---
+++ Client/src/main/java/de/hlg/oneUpGrader/client/ui/view/VerifizierenView.java
@@ -0,0 +1,8 @@
+package main.java.de.hlg.oneUpGrader.client.ui.view;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+/**
+ * Created by Jakob on 08.05.2017.
+ */
+public class VerifizierenView extends FXMLView {}
