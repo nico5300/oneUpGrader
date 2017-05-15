6b081e2a7724ab0c4bf55315ed881184c587085b
MichaelBonacina <michael.bonacina@outlook.de>
2017-05-15 10:23:18 +0200

    Create AnmeldenQuery.java

---
+++ Client/src/main/java/de/hlg/oneUpGrader/client/dbConnection/AnmeldenQuery.java
@@ -0,0 +1,50 @@
+package de.hlg.oneUpGrader.client.dbConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+/**
+ * Created by Michael on 12.05.2017.
+ */
+public class AnmeldenQuery {
+
+    private String query1 = "SELECT * FROM Anwender WHERE Email = '";
+    private String query2 = "' AND Passwort = '";
+    private String query3 = "';";
+
+    public boolean AnmeldenQuery(String username, String passwort) throws SQLException {
+        DbConnection datenbank = DbConnection.getInstance();
+
+        StringBuilder queryComplete = new StringBuilder();
+        queryComplete.append(query1);
+        queryComplete.append(username);
+        queryComplete.append(query2);
+        queryComplete.append(passwort);
+        queryComplete.append(query3);                       //Datenbank Abfrage aus Variablen und SQL zusammenbauen
+
+        Optional<PreparedStatement> optStatement = datenbank.getPreparedStatement(queryComplete.toString());
+        PreparedStatement statement = optStatement.get();
+        try {
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }                                                   //Abfrage ausführen oder Fehler
+
+        ResultSet ergebnis = statement.getResultSet();      //Ergebnistabelle der Abfrage
+
+        if(ergebnis.next())
+        {
+            String userergebnis = ergebnis.getString(0);
+            String passwortergebnis = ergebnis.getString(1);        //Auslesen des Ergebnisses
+
+            if(userergebnis == username && passwortergebnis == passwort)
+            {
+                return true;                                                    //Wenn user und passwort richtig -> true
+            }
+
+        }
+        return false;                                                           // sonst -> false
+    }
+}
