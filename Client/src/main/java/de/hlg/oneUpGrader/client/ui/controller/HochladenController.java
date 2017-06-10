8665d3414f553978c6cfb9127bed9acf17c84913
JustGamerz <jakobkr8@gmail.com>
2017-06-10 15:41:30 +0200

    HochladenController

--- Client/src/main/java/de/hlg/oneUpGrader/client/ui/controller/HochladenController.java
+++ Client/src/main/java/de/hlg/oneUpGrader/client/ui/controller/HochladenController.java
@@ -11,9 +11,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import javax.inject.Inject;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,12 +24,6 @@ import java.util.ResourceBundle;
  */
 @SuppressWarnings("unchecked")
 public class HochladenController implements Initializable{
-
-    //ObservableList<String> artList;
-
-    public HochladenController() {
-
-    }
 
     @FXML
     private Button btnBack;
@@ -92,9 +88,24 @@ public class HochladenController implements Initializable{
         stage1.show();
     }
 
+
+    /**
+     * @param ActionEvent
+     * @Author Jakob
+     *
+     *
+     *
+     *
+     */
     @FXML
     public void onPruefungAuswaehlenClick(ActionEvent e){
         //todo
+        File file = choosefile();
+        String st = file.getAbsolutePath();
+        System.out.println(st);
+        labelPruefungDatei.setText("hi");
+
+
     }
 
     @FXML
@@ -102,6 +113,11 @@ public class HochladenController implements Initializable{
         //todo @Jakob: Wenn du die HochladenQuery benutzen willst, ruf mich an und frag einfach
     }
 
+    /**
+     * @param url
+     * @param resourceBundle
+     * Diese Methode sorgt dafür, dass der cboxArt auswahlmöglichkeiten zur Verfügung gestellt werden.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         artList = FXCollections.observableArrayList();
@@ -110,5 +126,21 @@ public class HochladenController implements Initializable{
         artList.add("großer Leistungsnachweis");
 
         setCboxArtList(artList);
+    }
+
+    /**
+     *
+     * @Author Jakob
+     * @return File
+     *
+     * Diese Methode erlaubt es dem User, den Dateipfad der Hochzuladenden Datei mithilfe des Filechooer Dialogs auszuwählen.
+     */
+
+    public File choosefile() {
+        FileChooser filechooser = new FileChooser();
+        return filechooser.showOpenDialog(datepckDatum.getScene().getWindow());
+
+        //bei der Auswahl von mehreren Bildern zu verwenden --> Wunschkriterium?
+        //return filechooser.showOpenMultipleDialog(datepckDatum.getScene().getWindow());
     }
 }
