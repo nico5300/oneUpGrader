ef93efc34b83bc96d8e644c99249a41ece76a8cc
MichaelBonacina <michael.bonacina@outlook.de>
2017-07-13 07:01:37 +0200

    Verifizierer + Autor der Prüfung kriegen jetzt Pkt

--- Client/src/main/java/de/hlg/oneUpGrader/client/dbConnection/VerifizierenErgebnisSendenQuery.java
+++ Client/src/main/java/de/hlg/oneUpGrader/client/dbConnection/VerifizierenErgebnisSendenQuery.java
@@ -17,6 +17,8 @@ import java.util.Optional;
     Methode überprüft, ob nun bereits dreimal verifiziert
     Falls ja, Methode löscht alles aus verifiziert Tabelle
     UND trägt bei Prüfung ein ob Prüfung passt oder nicht
+    Verifizierer bekommt immer 2 Punkte
+    Falls Prüfung passt bekommt Autor der Prüfung Punkte (Klausur 5, Ex 3)
  */
 public class VerifizierenErgebnisSendenQuery extends Task<Void> {
 
@@ -24,6 +26,12 @@ public class VerifizierenErgebnisSendenQuery extends Task<Void> {
     String query2 = "SELECT * FROM Verifiziert WHERE PrüfungsID = ?";
     String query3 = "DELETE FROM Verifiziert WHERE PrüfungsID = ?";
     String query4 = "UPDATE Prüfungen SET Verifiziert = ? WHERE PrüfungsID = ?";
+    String query44 = "SELECT Punkte FROM Anwender WHERE Email = ?";
+    String query45 = "UPDATE Anwender SET Punkte = ? WHERE Email = ?";
+    String query5 = "SELECT Art, AutorID FROM Prüfungen WHERE PrüfungsID = ?";
+    String query6 = "SELECT Punkte FROM Anwender WHERE Email = ?";
+    String query7 = "UPDATE Anwender SET Punkte = ? WHERE Email = ?";
+    
 
     Boolean erg1 = false;
     Boolean erg2 = false;
@@ -32,11 +40,20 @@ public class VerifizierenErgebnisSendenQuery extends Task<Void> {
     String email;
     int TestID;
     Boolean ergebnis;
+    Boolean TestArt;
+    Boolean PVer;
+    int Pk;
+    int Pkv;
 
     public VerifizierenErgebnisSendenQuery(String e, int ID, boolean er) {
         email = e;
         TestID = ID;
         ergebnis = er;
+        TestArt = false;
+        AutorID = false;
+        Pver = false;
+        Pk = 0;
+        Pkv = 0;
     }
 
     protected Void call() throws SQLException {
@@ -87,11 +104,13 @@ public class VerifizierenErgebnisSendenQuery extends Task<Void> {
                         e.printStackTrace();
                     }
 
-                    Optional<PreparedStatement> opt4 = datenbank.getPreparedStatement(query3);
+                    Optional<PreparedStatement> opt4 = datenbank.getPreparedStatement(query4);
                     PreparedStatement queryComplete4 = opt4.get();
 
                     if ((erg1 && erg2) || (erg1 && erg3) || (erg2 && erg3)) {
                         queryComplete4.setBoolean(1, true);
+                        Pver = true;
+                        
                     } else {
                         queryComplete4.setBoolean(1, false);
                     }
@@ -102,8 +121,102 @@ public class VerifizierenErgebnisSendenQuery extends Task<Void> {
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
+                    
+                    
+                    //Punkte des Verifizierers holen
+                    Optional<PreparedStatement> opt44 = datenbank.getPreparedStatement(query44);
+                    PreparedStatement queryComplete44 = opt44.get();
 
+                    queryComplete44.setString(email);
+                    
+                    try {
+                        queryComplete44.execute();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    ResultSet ergebnis44 = queryComplete44.getResultSet();
+                    
+                    if(ergebnis44.next())
+                    {
+                       Pkv = ergebnis44.getInt("Punkte");     
+                    }
+                    Pkv = Pkv + 2;
+                    
+                    
+                    //Punkte des Verifizierers Updaten
+                    Optional<PreparedStatement> opt45 = datenbank.getPreparedStatement(query45);
+                    PreparedStatement queryComplete45 = opt45.get();
+                    
+                    queryComplete45.setInt(Pkv);
+                    queryComplete45.setString(email);
+                    
+                    try {
+                        queryComplete45.execute();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    
+                    
+                    
+                    //Hierfür neues Attribut AutorID mit email des Autors der Prüfung in Datenbank Tabelle Prüfungen notwendig!
+                    //Autor der Prüfung bekommt seine Punkte
+                    if(Pver)
+                    {
+                        Optional<PreparedStatement> opt5 = datenbank.getPreparedStatement(query5);
+                        PreparedStatement queryComplete5 = opt5.get();
+                    
+                        queryComplete5.setInt(TestID);
+                    
+                        try {
+                            queryComplete5.execute();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        ResultSet ergebnis5 = queryComplete5.getResultSet();    
+                        if(ergebnis5.next())
+                        {
+                            TestArt = ergebnis5.getBoolean("Art");
+                            AutorID = ergebnis5.getString("AutorID");
+                        }
 
+                    
+                        Optional<PreparedStatement> opt6 = datenbank.getPreparedStatement(query6);
+                        PreparedStatement queryComplete6 = opt6.get();
+                        queryComplete6.setString(AutorID);
+                        
+                        try {
+                        queryComplete6.execute();
+                        } catch (SQLException e) {
+                        e.printStackTrace();
+                        }
+                        ResultSet ergebnis6 = queryComplete6.getResultSet(); 
+                        
+                        if(ergebnis6.next())
+                        {
+                            Pk = ergebnis6.getInt("Punkte");
+                        }
+                        
+                        if(TestArt)
+                        {
+                            Pk = Pk + 5;
+                        }
+                        else 
+                        {
+                            PK = Pk + 3;
+                        }  
+                        
+                        Optional<PreparedStatement> opt7 = datenbank.getPreparedStatement(query7);
+                        PreparedStatement queryComplete7 = opt7.get();
+                        queryComplete7.setInt(Pk);
+                        queryComplete7.setString(AutorID);
+                        
+                        try {
+                        queryComplete7.execute();
+                        } catch (SQLException e) {
+                        e.printStackTrace();
+                        }
+                        
+                    }    
                 }
             }
         }                                                                   // Wenn Prüfung dreimal verifiziert
